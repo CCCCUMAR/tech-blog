6cb25caf8b0e26c545594689660d7bcf792fffb3
Cumar Cumar <umar-19@live.com>
2022-05-22 14:14:18 -0500

    added npm package requirments in server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+// set up variables to require npms
+const path = require('path')
+const express = require('express')
+const session = require('express-session')
+const handlebars = require('expresss-handlebars')
+require('dotenv').config();
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
